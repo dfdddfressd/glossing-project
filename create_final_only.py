46dfcd04a110bff834236a4b559831490858f7db
Micha Elsner <melsner@ling.ohio-state.edu>
2024-10-23 10:37:49 -0400

    repair mode for finding missing content

--- create_final_only.py
+++ create_final_only.py
@@ -5,8 +5,11 @@ from collections import Counter, defaultdict
 import os
 import re
 import json
-from json_scanner import JSONScanner
 
+from nltk.stem import WordNetLemmatizer as wnl
+import nltk
+
+from json_scanner import JSONScanner
 from searching_string import *
 from sentence_split import *
 from glosser import glossSentence
@@ -107,7 +110,7 @@ def llmGlosses(llm, language, index, dicts, sent, translation, noLM=False, conve
     
     return gloss
 
-def createFinalTranscripts(language, devSents, finalSelection):
+def createFinalTranscripts(language, devSents, repairs, finalSelection):
     total = ""       
 
     for index, group in enumerate(devSents):
@@ -134,6 +137,8 @@ def createFinalTranscripts(language, devSents, finalSelection):
 
             elif finalSelection == "llm":
                 sentenceGlosses = llmGlosses(llm, language, index, x, sentence, translation, noLM=False, convert_from_numeric=True)
+            elif finalSelection == "repair":
+                sentenceGlosses = repairedGlosses(language, index, x, repairs)
             else:
                 assert(0), f"Unknown final selection method {finalSelection}"                    
         except:
@@ -148,18 +153,224 @@ def createFinalTranscripts(language, devSents, finalSelection):
     with open("final.txt", "w", encoding="utf-8") as file:
         file.write(total)
 
+def findUncertainWords(sentGroup, langInfo):
+    (language, sentences, wordToSentence, subToWord, wordToTags, metaIndices) = langInfo
+    sent, gloss, trans, _ = sentGroup
+    uncertain = []
+
+    for index, word in enumerate(sentence_split(sent)):
+        freqTags, freqFeats = frequentTags(word, wordToSentence)
+        if len(freqTags) == 0:
+            approxMatches = lcsMatch(word, subToWord, wordToSentence, length=4)
+            if len(approxMatches) == 0:
+                uncertain.append(index)
+
+    return uncertain
+
+def missingWordPrompt(sentGroup, dicts, promptTemplate="uncertain_word_template.txt"):
+    with open(promptTemplate, "r") as originalfh:
+        text = "".join(originalfh.readlines())
+
+    sent, goldGloss, trans, _ = sentGroup
+
+    transWords = sentence_split(trans)
+    posTags = nltk.pos_tag(transWords)
+
+    def contentful(word, tag):
+        #noun, verb, adverb, adjective
+        if any([tag.startswith(letter) for letter in "NVRJ"]):
+            lemmas = lemmatize(word, wnl())
+            if any([lemma in ["be", "have"] for lemma in lemmas]):
+                return False
+
+            return True
+
+        return False
+
+    contentWords = [wi for (wi, tag) in posTags if contentful(wi, tag)]
+
+    resultFormat = [f"{{ \"word\" : \"{wi}\", \"status\" : \"_\" }}"
+                    for wi in contentWords]
+    resultFormat = "[\n" + ",\n".join(resultFormat) + "\n]\n"
+
+    sentenceGlosses = []
+    for myDict in dicts:
+        glosses = myDict["glosses"]
+        sentenceGlosses.append(".".join(glosses[0].split()))
+
+    instanceDict = {
+        "SENT" : sent,
+        "GLOSS" : "\t".join(sentenceGlosses),
+        "TRANS" : trans,
+        "RESULT" : resultFormat,
+        }
+
+    filledPrompt = text.format(**instanceDict)
+    return filledPrompt
+
+def repairPrompt(sentGroup, dicts, uncertain, missing, promptTemplate="repair_word_prompt.txt"):
+    lemmatizer = wnl()
+    sent, goldGloss, trans, _ = sentGroup
+    split = sentence_split(sent)
+    if len(split) != len(dicts):
+        print("Output length is", len(dicts), "but sent length is", len(split), "for",
+              sent)
+        for ind, di in enumerate(dicts):
+            print(di)
+    assert(len(split) == len(dicts))
+    options_per_word = [ [] for si in split]
+    ourGloss = []
+    for ind, di in enumerate(dicts):
+        glosses = di["glosses"]
+        gloss = (".".join(glosses[0].split()))
+        ourGloss.append(gloss)
+        options_per_word[ind].append(gloss)
+
+    missingLemmas = []
+    for word in missing:
+        lemmas = lemmatize(word, lemmatizer)
+        best = min(lemmas, key=len)
+        missingLemmas.append(best)
+
+    for ind in uncertain:
+        for mi in missingLemmas:
+            options_per_word[ind].append(mi)
+
+    formatted_gloss_per_word = []
+    for pos, (word, di, options) in enumerate(zip(split, dicts, options_per_word)):
+        if len(options) > 1:
+            candidates = "[ " + ", ".join([f"\"{opt}\"" for opt in options]) + "]"
+            json = f"{{ \"word\" : \"{word}\", \"word_pos\" : {pos}, \"candidates\" : {candidates}, \"select\" : _, }}"
+            formatted_gloss_per_word.append(json)
+
+    formatted_gloss_per_word = "[\n" + ",\n".join(formatted_gloss_per_word) + "\n]"
+
+    substitutions = {
+        "SENTENCE" : sent,
+        "GLOSS" : "\t".join(ourGloss),
+        "TRANSLATION" : trans,
+        "GLOSS_PER_WORD" : formatted_gloss_per_word,
+                     }
+
+    with open(promptTemplate, "r" ) as originalfh:
+        text = "".join(originalfh.readlines())
+
+    filledPrompt = text.format(**substitutions)
+
+    return filledPrompt
+
+def repairTranscripts(devSents, langInfo, noLM=False):
+    repairSent = set()
+
+    for index, sent in enumerate(devSents):
+        dicts = createFinal(f"outputs/{language}/{index}/output.txt")
+        uncertain = findUncertainWords(sent, langInfo)
+
+        prompt = missingWordPrompt(sent, dicts)
+        outFile = f"prompts/{language}/{index}/missing.txt"
+        with open(outFile, "w", encoding="utf-8") as ofh:
+            ofh.write(prompt)
+
+        if not noLM:
+            prompt_template = ChatPromptTemplate.from_template(
+                template="You are a helpful assistant. {prompt}"
+            )
+            chain = LLMChain(llm=llm, prompt=prompt_template)
+            response = chain.run(llm=llm, prompt=prompt)
+            outFile = f"outputs/{language}/{index}/missing.txt"
+            with open(outFile, "w", encoding="utf-8") as ofh:
+                ofh.write(response)
+
+            dec = JSONScanner(response)
+            missingWords = dec.scan()
+        else:
+            #load the missing file if there is one
+            mFile = f"outputs/{language}/{index}/missing.txt"
+            try:
+                with open(mFile, encoding="utf-8") as ifh:
+                    response = ifh.read()
+
+                dec = JSONScanner(response)
+                missingWords = dec.scan()
+            except FileNotFoundError:
+                missingWords = []
+            except StopIteration:
+                print("Cannot read", mFile)
+                raise
+
+        missing = set()
+        for wDict in missingWords:
+            if wDict["status"] == "check":
+                missing.add(wDict["word"].lower())
+
+        #print("missing words:", missing)
+
+        #don't bother trying to repair the gloss unless we have a missing meaning
+        #and a gloss element for which the direct evidence was poor
+        if len(uncertain) > 0 and len(missing) > 0:
+            repairSent.add(index)
+            prompt = repairPrompt(sent, dicts, uncertain, missing)
+            outFile = f"prompts/{language}/{index}/repair.txt"
+            with open(outFile, "w", encoding="utf-8") as ofh:
+                ofh.write(prompt)
+
+            if not noLM:
+                prompt_template = ChatPromptTemplate.from_template(
+                    template="You are a helpful assistant. {prompt}"
+                )
+                chain = LLMChain(llm=llm, prompt=prompt_template)
+                response = chain.run(llm=llm, prompt=prompt)
+                outFile = f"outputs/{language}/{index}/repair.txt"
+                with open(outFile, "w", encoding="utf-8") as ofh:
+                    ofh.write(response)
+
+    return repairSent
+
+def repairedGlosses(language, index, dicts, repairSent):
+    #if no repairs are possible for this sentence, don't bother looking for an llm choice
+    if index in repairSent:
+        repairFile = f"outputs/{language}/{index}/repair.txt"
+        with open(repairFile, encoding="utf-8") as ifh:
+            response = ifh.read()
+
+        dec = JSONScanner(response)
+        repairChoices = dec.scan()
+    else:
+        repairChoices = []
+
+    print("repairing sentence", index, "with", repairChoices)
+
+    sentenceGlosses = []
+    for ind, myDict in enumerate(dicts):
+        glosses = myDict["glosses"]
+        gloss = ".".join(glosses[0].split())
+        for ri in repairChoices:
+            if ri["word_pos"] == ind:
+                repair = ri["select"]
+                candidates = ri["candidates"]
+                print("checking candidates for", ri, repair)
+
+                if repair > 0:
+                    print("replacing", gloss, "with", candidates[repair])
+                    gloss = candidates[repair]
+
+        sentenceGlosses.append(gloss)
+
+    return sentenceGlosses
+
 if __name__ == "__main__":
     #pull the language from the command line argument array
     language = sys.argv[1]
 
-    finalSelection = "first"
+    finalSelection = "repair"
 
-    if finalSelection == "llm":
-        #llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.25)
-        llm = ChatOpenAI(model="gpt-4o", temperature=0.25)
-        #llm = Ollama(model="llama2")
+    #llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.25)
+    llm = ChatOpenAI(model="gpt-4o", temperature=0.25)
+    #llm = Ollama(model="llama2")
     
     langInfo = readLanguage(language)
     (_, devSents, _, _, _, _) = readLanguage(language, split="debug")
 
-    createFinalTranscripts(language, devSents, finalSelection)
+    langInfo = readLanguage(language)
+    repairs = repairTranscripts(devSents, langInfo, noLM=False)
+    createFinalTranscripts(language, devSents, repairs, finalSelection)
