9f799a2f1542fda0e29aebdddc892164d510b599
Micha Elsner <melsner@ling.ohio-state.edu>
2024-12-31 09:59:52 -0500

    gloss_all now has arguments from cmd line

--- gloss_all.py
+++ gloss_all.py
@@ -12,34 +12,65 @@ from glosser import createFinal
 from create_final_only import createFinalTranscripts
 import os
 
-if __name__ == "__main__":
-    #os.environ["OPENAI_API_KEY"] = ""
-    #llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.25)
-    llm = ChatOpenAI(model="gpt-4o", temperature=0.25)
-    #llm = Ollama(model="llama2")
-    #llm = None
-    prompt_template = ChatPromptTemplate.from_template(
-        template="You are a helpful assistant. {prompt}"
-    )
+import argparse
 
-    #pull the language from the command line argument array
-    language = sys.argv[1]
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--language")
+    parser.add_argument("--split", default="debug")
+    parser.add_argument("--use_lm", action="store_true")
+    parser.add_argument("--instructions", default="0", help="Number of the instruction dir, or best, or range (0:10).")
+    args = parser.parse_args()
+    return args
 
-    langInfo = Information(language)
-    devInfo = Information(language, split="debug")
-    
-    for count, sentence in enumerate(devInfo.sentences):
-        promptPath = f"prompts/{language}/{count}"
-        outputPath = f"outputs/{language}/{count}"
+def glossAllSentences(info, llm, prompt_template, use_lm, instructions, subdir=False):
+    for count, sentence in enumerate(info.sentences):
+        if subdir:
+            #make an instruction-set-specific subdirectory
+            promptPath = f"prompts/{info.language}/i{instructions}/{count}"
+            outputPath = f"outputs/{info.language}/i{instructions}/{count}"
+        else:
+            promptPath = f"prompts/{info.language}/{count}"
+            outputPath = f"outputs/{info.language}/{count}"
+
         os.makedirs(promptPath, exist_ok=True)
         os.makedirs(outputPath, exist_ok=True)
-        
+
+        print(f"...{count}")
+
         glossSentence(sentence, langInfo,
                       llm=llm,
                       prompt_template=prompt_template,
                       promptPath=promptPath,
                       outputPath=outputPath,
                       templatePath="originalfile.txt",
-                      noLM=True)
+                      instructions=instructions,
+                      noLM=not args.use_lm)
 
-    createFinalTranscripts(language, devInfo, finalSelection="first")
+if __name__ == "__main__":
+    #pull the language from the command line argument array
+    args = get_args()
+
+    if args.use_lm:
+        llm = ChatOpenAI(model="gpt-4o", temperature=0.0)
+        prompt_template = ChatPromptTemplate.from_template(
+            template="You are a helpful assistant. {prompt}"
+        )
+    else:
+        llm = None
+        prompt_template = None
+
+    language = args.language
+
+    langInfo = Information(language)
+    devInfo = Information(language, split=args.split)
+
+    if ":" in args.instructions:
+        instrRange = range(int(args.instructions[:args.instructions.index(":")]),
+                           int(args.instructions[args.instructions.index(":") + 1:]))
+        for instr in instrRange:
+            glossAllSentences(devInfo, llm, prompt_template, args.use_lm, instructions=instr, subdir=True)
+            createFinalTranscripts(language, devInfo, finalSelection="first", output_name=f"final_i{instr}.txt", instructions=instr)
+    else:
+        glossAllSentences(devInfo, llm, prompt_template, args.use_lm, instructions=args.instructions)
+        createFinalTranscripts(language, devInfo, finalSelection="first")
